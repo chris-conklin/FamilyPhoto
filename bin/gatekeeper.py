a53dcb75dae4a846b6c3d4c55d50d682b6d284f0
chris-conklin <cconklin2376@gmail.com>
2017-02-25 11:39:56 -0500

    Added header to gatekeeper report Modified text output to be more descriptive Added counters to track errors and moves Added file moving functionality

--- bin/gatekeeper.py
+++ bin/gatekeeper.py
@@ -2,22 +2,29 @@ import os
 import time
 import datetime
 
-#app_path = '/srv/FamilyPhoto/'
-#dropoff_path = app_path + 'dropoff/'
-app_path = '/Users/cconklin/src/FamilyPhoto/'
+app_path = '/srv/FamilyPhoto/'
 dropoff_path = app_path + 'dropoff/'
-logpath = app_path + 'log/'
+logpath = app_path + 'log/gatekeeper/'
+store_path = app_path + 'store/'
 
+if(len(os.listdir(dropoff_path))) > 0:
+    move_cnt = 0
+    warn_cnt = 0
+    millis = int(round(time.time() * 1000))
+    today = str(datetime.date.today())
+    logname = 'gatekeeper_' + today + '_' +  str(millis) + '.log'
+    logfile = open(logpath + logname, 'w') 
+    logfile.write(" %sbin/gatekeeper.py           Gatekeeper Processing Log      Date:  %s\n\n"%(app_path,today)) 
 
-millis = int(round(time.time() * 1000))
-logname = 'gatekeeper_' + str(datetime.date.today()) + '_' +  str(millis) + '.log'
-logfile = open(logpath + logname, 'w') 
+    for item in os.listdir(dropoff_path):
+        if os.path.isfile(dropoff_path + item):
+	    os.rename(dropoff_path + item, store_path + item)
+	    logfile.write("Moved %s%s to %s%s"%(dropoff_path,item,store_path,item))
+	    move_cnt += 1
+        else:
+            logfile.write("WARNING: Found a directory '%s' in dropoff items. Please breakdown the directory into files to be added to the application\n"%(item))
+            warn_cnt += 1
+    logfile.write("\n\nMoved %d items from dropoff to store."%(move_cnt))
+    logfile.write("\n\nThere were %d issues found."%(warn_cnt))
 
-
-for item in os.listdir(dropoff_path):
-    if os.path.isfile(dropoff_path + item):
-        logfile.write("Found a file " + item)
-    else:
-        logfile.write("Found a directory " + item)
-
-
+    logfile.close()
